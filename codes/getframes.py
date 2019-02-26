53965d14188c1c533f0f7c7793e7ab0550298b5a
Vítor Yeso <vitoryeso@outlook.com>
2019-02-26 23:31:01 +0000

    get and split frames

---
+++ codes/getframes.py
@@ -0,0 +1,48 @@
+
+
+import cv2 
+from sklearn.model_selection import train_test_split
+
+# Function to extract frames 
+def FrameCapture(path): 
+        vidObj = cv2.VideoCapture(path) 
+        fps = vidObj.get(cv2.CAP_PROP_FPS)
+        
+        # checks whether frames were extracted 
+        success = True
+        aux = count = 0
+        print('fps: {}'.format(fps))
+        df = []
+        while success:
+                success, image = vidObj.read() 
+                if aux%7==0:
+                        df.append(image)
+                        count +=1
+                aux +=1
+        print('{} frames salvos'.format(count))
+        t_size = float(input('digite a porcentagem de teste: '))
+        t_size = t_size/100.0
+        X_train, X_test = train_test_split(df, test_size=t_size)
+        print('tamanho do train: {}'.format(len(X_train)))
+        print('tamanho do test: {}'.format(len(X_test)))
+        flag='eu'
+        for i in range(len(X_train)):
+                if flag=='eu':
+                        cv2.imwrite('train/eu/frame{}.jpg'.format(i), X_train[i])
+                        flag = 'mateus'
+                else:
+                        if flag=='mateus':
+                                cv2.imwrite('train/mateus/frame{}.jpg'.format(i), X_train[i])
+                                flag='lucas'
+                        else:
+                                cv2.imwrite('train/lucas/frame{}.jpg'.format(i), X_train[i])
+                                flag='eu'
+
+
+
+
+        for i in range(len(X_test)):
+                cv2.imwrite('test/frame{}.jpg'.format(i+79), X_test[i])
+        
+
+FrameCapture("DJI_0055.MP4") 
